0165ffe0545746920a8e314e1577c655e0594ddf
huhsiyoung <july433@naver.com>
2026-02-05 00:48:13 +0900

    프로젝트 초기 설정

---
+++ src/test/java/site/ticketlion/TicketlionApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ticketlion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketlionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
